3ad29ba4434d7a7f98bce152577b30afd3c0d11c
Samantha <samantha.chhoeu@gmail.com>
2017-04-29 14:28:20 +1000

    start - algorithm for simple case

---
+++ merge.py
@@ -0,0 +1,10 @@
+# BASIC CASE - two playlists with even length and no duplicates
+
+# playlist A
+# playlist B
+# zipped playlist
+
+# shuffle A
+# shuffle B
+
+# go through playlists and add to zipped playlist
