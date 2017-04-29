46b86759ee5eafa23adec20da0323db5a0785b91
Samantha <samantha.chhoeu@gmail.com>
2017-04-29 16:46:54 +1000

    removes duplicates before zipping
    going to work on making a new list when it runs out

--- merge.py
+++ merge.py
@@ -2,11 +2,27 @@
 
 import random
 
+# remove duplicates in two playlists
+def remove_dups(A,B):
+    seen = {}
+    for elem in A:
+        if elem not in seen.keys():
+            seen[elem] = 1
+        else:
+            A.remove(elem)
+    for elem in B:
+        if elem not in seen.keys():
+            seen[elem] = 1
+        else:
+            B.remove(elem)
+
+
 # playlist A
-A = ["A1","A2","A3","A4","A5","dup"]
+A = ["A1","A2","A3","A4","A5","dup", "A6"]
 # playlist B
 B = ["B1","B2","B3","B4","B5","dup"]
 
+remove_dups(A,B)
 print A
 print B
 
@@ -18,12 +34,21 @@ random.shuffle(A)
 # shuffle B
 random.shuffle(B)
 
+def merge_diff_length(list1,list2):
+    num = min(len(list1), len(list2))
+    result = [None]*(num*2)
+    result[::2] = list1[:num]
+    result[1::2] = list2[:num]
+    result.extend(list1[num:])
+    result.extend(list2[num:])
+    print result
+
+
 # go through playlists and add to zipped playlist
-for i in range (0,len(A)):
-    if A[i] not in zipped:
+def merge_same_length():
+    for i in range (0,len(A)):
         zipped.append(A[i])
-    if B[i] not in zipped:
         zipped.append(B[i])
+    print zipped
 
-
-print zipped
+merge_diff_length(A,B)
