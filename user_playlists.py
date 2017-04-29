dc6eeeb5e78894e36cb74bbcd6ab53b7294029f1
yuvii28 <yuvraj28singh@hotmail.com>
2017-04-30 02:23:35 +1000

    uh got some new shiz going on

--- user_playlists.py
+++ user_playlists.py
@@ -1,33 +1,56 @@
-# shows a user's playlists (need to be authenticated via oauth)
-
-import pprint
 import sys
-import os
-import subprocess
-
 import spotipy
-
 import spotipy.util as util
-import glob
+import json
 
-results = {}
+from read_a_playlist import getTracks
+from merge import merge
+from user_playlists import getPlaylists
 
-if len(sys.argv) > 1:
-    username = sys.argv[1]
-else:
-    print("Whoops, need your username!")
-    print("usage: python user_playlists.py [username]")
-    sys.exit()
+scope = 'playlist-modify-public'
 
-token = util.prompt_for_user_token(username)
+#Initial Call Main.py Userid 
+def findPlayLists(username = None):
+    if username is not None:
+        #note username is actually the userid
+        playlist_options = getPlaylists(username)
+        #print playlist_options
+        return playlist_options
+    else:
+        print("Usage: %s username ..." % (sys.argv[0],))
+        sys.exit()
+        return
 
-if token:
-    sp = spotipy.Spotify(auth=token)
-    playlists = sp.user_playlists(username)
-    for playlist in playlists['items']:
-        results[playlist['name']] = playlist['id']
-else:
-	print("Can't get token for", username)
+def makePlaylist(username, playlist_name, playlist_url):
+    #Display options -> Front end
+    #Track 1 = Playlist id -> need to use id to get list of tracks
+    track1 = getTracks(name=username, playlist_input=playlist_name) 
+    #Track 2 = url -> need to use url to get list of tracks
+    track2 = getTracks(name=playlist_url)
 
-print results.keys()
-os.remove(".cache-" + username)
+    #merge playlists
+    # get a list of songs that will be added into the new playlist
+    track_ids = merge(track1, track2)
+
+    token = util.prompt_for_user_token(username, scope)
+
+    if token:
+    #userid = sp.me()['uri'] someone's user id must be processed before this function
+        sp = spotipy.Spotify(auth=token)
+        sp.trace = False
+
+    # # #create a new playlist for yourself
+        playlists = sp.user_playlist_create(username, playlist_name, public=True)
+
+    # get playlist id
+        plid = playlists['id']
+
+    # # Add songs
+        results = sp.user_playlist_add_tracks(username, plid, track_ids)
+        print(results)
+        return results
+
+    else:
+        print("Can't get token for", username)
+
+findPlayLists(username='12186663114')
