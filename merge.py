f3ea7b884c05bf1d95499b3606b1ef36d97a3589
Samantha <samantha.chhoeu@gmail.com>
2017-04-29 15:59:07 +1000

    "zip" handles duplicates by not adding duplicates

--- merge.py
+++ merge.py
@@ -3,9 +3,12 @@
 import random
 
 # playlist A
-A = ["A1","A2","A3","A4","A5"]
+A = ["A1","A2","A3","A4","A5","dup"]
 # playlist B
-B = ["B1","B2","B3","B4","B5"]
+B = ["B1","B2","B3","B4","B5","dup"]
+
+print A
+print B
 
 # zipped playlist
 zipped = []
@@ -17,9 +20,10 @@ random.shuffle(B)
 
 # go through playlists and add to zipped playlist
 for i in range (0,len(A)):
-    zipped.append(A[i])
-    zipped.append(B[i])
+    if A[i] not in zipped:
+        zipped.append(A[i])
+    if B[i] not in zipped:
+        zipped.append(B[i])
+
 
 print zipped
-
-# shows a user's playlists (need to be authenticated via oauth)
